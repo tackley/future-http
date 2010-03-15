7d8675c24aae7827930859611f6d7371efdb8e9a
Graham Tackley <graham.tackley@guardian.co.uk>
2010-03-15 16:02:00 +0000

    Added test using actors and callbacks

--- src/test/scala/ParallelisationTest.scala
+++ src/test/scala/ParallelisationTest.scala
@@ -1,12 +1,18 @@
+import actors.TIMEOUT
 import io.{Source}
+import java.lang.Throwable
 import org.eclipse.jetty.client.{ContentExchange, HttpExchange, HttpClient}
+import org.eclipse.jetty.io.Buffer
 import scala.actors.Futures._
+import scala.actors.Actor._
 import java.net.URL
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.{BeforeAndAfterAll, GivenWhenThen, Assertions, FunSuite}
 
 
 class ParallelisationTest extends FunSuite with Assertions with ShouldMatchers with TestJettyServlets {
+  scala.actors.Debug.level = 5
+
   ignore("simple futures with java URL") {
     val futureList = for (i <- 1 to 100) yield {
       future {
@@ -23,7 +29,7 @@ class ParallelisationTest extends FunSuite with Assertions with ShouldMatchers w
     }
   }
 
-  test("simple waitForDone with jetty http client") {
+  ignore("simple waitForDone with jetty http client") {
     val client = new HttpClient
     client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL)
     client.start
@@ -42,12 +48,95 @@ class ParallelisationTest extends FunSuite with Assertions with ShouldMatchers w
 
     for (e: ContentExchange <- exchangeList) {
       e.waitForDone match {
-        case HttpExchange.STATUS_COMPLETED => println("back on the main thread: "+ e.getResponseContent)
-        case other => println("unexpected status: "+ other)
+        case HttpExchange.STATUS_COMPLETED => println("back on the main thread: " + e.getResponseContent)
+        case other => println("unexpected status: " + other)
       }
     }
 
   }
 
+
+  ignore("how do I reply to a non actor thread from an actor") {
+    val a = actor {
+      loop {
+        react {
+          case anything => {
+            println("hey i got: " + anything + " from " + sender)
+            Thread.sleep(500)
+            println("i'm done")
+            //reply("response from: "+ anything)
+            sender ! "response from: " + anything
+          }
+        }
+      }
+
+      println("bye from the actor")
+    }
+
+    a ! "hello?"
+    a ! "hello again?"
+    println("that's all my messages sent")
+
+    receiveWithin(2000) {
+      case TIMEOUT => println("timeout!")
+      case s => println("received: " + s)
+    }
+
+  }
+
+  case class EXIT()
+
+  test("http request with callback") {
+
+    val client = new HttpClient
+    client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL)
+    client.start
+
+    val httpRequestActor = actor {
+      loop {
+        react {
+          case urlToHit: String => {
+            println("invoking: " + urlToHit + "...")
+
+            val exchange = new HttpExchange {
+              val replyTo = sender
+
+              override def onResponseStatus(version: Buffer, status: Int, reason: Buffer) = {
+                val s = "onResponseStatus " + urlToHit + ": " + status
+                replyTo ! s
+              }
+
+
+              override def onConnectionFailed(x: Throwable) = {
+                println("connection failed for "+ urlToHit + ": " + x)
+              }
+            }
+
+            exchange.setURL(urlToHit)
+
+            client.send(exchange)
+            println("exchange started")
+
+          }
+          case EXIT => println("exiting"); exit
+        }
+      }
+    }
+
+
+    for (i <- 1 to 100) {
+      httpRequestActor ! "http://localhost:8080/slow?i=" + i
+    }
+
+
+    var finished = false
+    while(!finished) {
+      receiveWithin(5000) {
+        case TIMEOUT => println("timeout!"); finished = true
+        case s => println("received: " + s)
+      }
+    }
+
+  }
 
 }
